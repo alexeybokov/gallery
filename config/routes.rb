b1a58a1afaca69f7a3366471e6ac2c6f73bdf781
alexeybokov <lexabokov18@gmail.com>
2018-11-28 17:12:24 +0200

    update config

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated do
+    root to: "secret#index", as: :authenticated_root
+  end
+
   scope '(:locale)' do
     resources :images do
       resources :comments, only: :create

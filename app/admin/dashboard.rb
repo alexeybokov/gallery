003612a3ff6d069ebd59f999889fc31f6ea5104e
alexeybokov <lexabokov18@gmail.com>
2018-12-13 18:25:09 +0200

    refactor admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,49 +1,49 @@
-ActiveAdmin.register_page "Dashboard" do
+ActiveAdmin.register_page 'Dashboard' do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: proc{ I18n.t('active_admin.dashboard') }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: proc { I18n.t('active_admin.dashboard') } do
+
+    section 'Recently 5 Categories' do
+      table_for Category.last(5) do |category|
+        category.column(:id)
+        category.column(:title) do |el|
+          link_to(el.title, admin_all_category_path(el))
+        end
+        category.column(:user_id) do |elem|
+          link_to(elem.user&.email, admin_user_path(elem.user))
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    columns do
-      column do
-        panel "Recent 5 Category" do
-          ul do
-            Category.last(5).map do |category|
-              li link_to(category.title, categories_path(category))
-            end
-          end
+    section 'Recently 5 Comments' do
+      table_for Comment.last(5).map do |comment|
+        comment.column(:id)
+        comment.column(:body) do |item|
+          link_to(item.body, admin_comment_path(item))
         end
-
-        panel "Recently 5 Comment" do
-          ul do
-            Comment.last(5).map do |comment|
-              li link_to(comment.body, image_comments_path(comment))
-            end
-          end
+        comment.column(:image_id) do |elem|
+          link_to(elem.image&.name, admin_image_path(elem.image))
         end
-
-        panel "Recently 10 Images" do
-          ul do
-            Image.last(5).map do |image|
-              li link_to(image.name, images_path(image))
-            end
-          end
+        comment.column(:user_id) do |elem|
+          link_to(elem.user&.email, admin_user_path(elem.user))
         end
       end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
     end
-  end # content
+
+    section 'Recently 10 Images' do
+      table_for Image.last(10) do |image|
+        image.column(:id)
+        image.column 'Image' do |img|
+          image_tag(img.picture.url, size: '60x40')
+        end
+        image.column(:name) do |item|
+          link_to(item.name, admin_image_path(item))
+        end
+        image.column(:category_id) do |elem|
+          link_to(elem.category&.title, admin_all_category_path(elem.category))
+        end
+      end
+    end
+  end
 end

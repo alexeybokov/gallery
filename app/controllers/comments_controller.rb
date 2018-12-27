a4d0526dc26b1cf7baa96cf1345deebeb83eb45e
alexeybokov <lexabokov18@gmail.com>
2018-12-27 18:33:52 +0200

    update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,14 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_image, only: :create
+  before_action :find_image, only: %i[new create]
 
   def index
     @comments = Comment.order(created_at: :desc).page(params[:page])
     record_activity('navigation')
+  end
+
+  def new
+    @comment = @image.comments.new
   end
 
   def create
